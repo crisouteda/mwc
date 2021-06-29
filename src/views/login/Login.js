f280cba007f68c5d0b962e06e101ba05446ed105
cristina <cristina.outeda.rua@gmail.com>
2021-06-29 16:14:38 +0200

    main page

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 import LoginForms from './LoginForms'
-import {Box, Heading} from '@chakra-ui/react'
+import {Box, Heading, Text} from '@chakra-ui/react'
+
 import Layout from '../../layout'
 
 export function Login() {
+
+    const history = useHistory()
+
     return(
         <>
             <Layout>
@@ -12,6 +17,7 @@ export function Login() {
                         Login
                     </Heading>
                     <LoginForms/>
+                    <Text>Still not a member? <Text as="span" color="blue" fontWeight="bold" cursor="pointer" onClick={() => history.push("/register")}>Register </Text></Text>
                 </Box>
             </Layout>
         </>
